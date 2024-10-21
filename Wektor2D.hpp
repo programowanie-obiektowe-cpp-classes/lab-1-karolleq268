788fe31da24fc91e0a54971ef1c301abd170fe92
karolleq268 <karolleq268@proton.me>
2024-10-21 20:36:06 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,49 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+    Wektor2D() : x(0), y(0) {}
+    Wektor2D(double a, double b) : x(a), y(b) {}
+
+    ~Wektor2D()
+    {
+        //cout << "Goodbye:\n";
+    }
+    
+    void setX(double a=0) 
+    {
+        x = a;
+    }
+    
+    void setY(double b=0) 
+    {
+        y = b;
+    }
+    
+    double getX() 
+    {
+        return x;
+    }
+
+    double getY() 
+    {
+        return y;
+    }
+
+    void print() 
+    {
+        cout<<"Wspolzedna x: "<<x<<endl<<"Wspolzedna y: "<<y<<endl;
+    }
+
+    double x;
+    double y;
 };
+
+Wektor2D operator+(Wektor2D w1, Wektor2D w2)
+{
+    return Wektor2D{w1.x + w2.x, w1.y + w2.y};
+}
+
+int operator*(Wektor2D w1, Wektor2D w2)
+{
+    return w1.x * w2.x + w1.y * w2.y;
+}
